091e88aefc68f14883e42663188c5e264d0d8d95
raresmigea <raresini@yahoo.com>
2020-04-10 22:40:12 +0300

    flow

--- public/chat.js
+++ public/chat.js
@@ -21,3 +21,13 @@ btn.addEventListener('click', function() {
 socket.on('chat', function(data) {
   output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>'; //output the data to the DOM
 })
+
+/*
+When we click send -> chat.js/btn.addEventListener - is listening for that click
+- grab the socket and emit a chat meesage - send it to the server
+- server (index.js/io.on - socket.on) - listens to all its sockets - when hear something
+- fires the callback fuction
+- io.sockets.emit() - grab all the sockets and emit that chat message and send it
+- then in FE: put it on the chat window:
+- client (chat.js/socket.on) socket.on - output.innerHTML - on the page
+*/
