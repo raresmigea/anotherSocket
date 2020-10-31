7c0565b97572e3287b4b5307094c544e0d5a0194
raresmigea <raresini@yahoo.com>
2020-10-31 16:14:23 +0200

    refactored index.js

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 import express, { static } from 'express';
 import socket from 'socket.io';
 
-//App setup
+// App setup
 const app = express();
 const server = app.listen(4000, () => {
   console.log('listening on port 4000');
 });
 
-//static files
+// static files
 app.use(static('public'));
 
-//socket setup
+// socket setup
 var io = socket(server);
 
-//listen for connection, when connection is made to the server - the callback function fires
-//the callback has an argument - socket - each client will have its own socket
+// listen for connection, when connection is made to the server - the callback function fires
+// the callback has an argument - socket - each client will have its own socket
 io.on('connection', (socket) => {
-  console.log('made socket connection', socket.id); //the socket.is different for each refresh
+  console.log('made socket connection', socket.id); // the socket.is different for each refresh
 
-  //listen for the message from the client
+  // listen for the message from the client
   socket.on('chat', (data) => {
-    // data- received from the client
+    // data - received from the client
     io.sockets.emit('chat', data); //we send it out to all the clients connected
   });
 
-  //listening for typing even
+  // listening for typing even
   socket.on('typing', (data) => {
     socket.broadcast.emit('typing', data); // broadcast - send to all but the sender
   });
